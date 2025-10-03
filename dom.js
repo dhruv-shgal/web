990e5b0012f43ce742e87e876608dafe0f692bc6
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-10-03 22:13:44 +0530

    Initial commit

---
+++ dom.js
@@ -0,0 +1,7 @@
+let colrChange = document.getElementById("name");
+let btn=document.getElementById("change")
+btn.addEventListener("click",function(){
+    colrChange.style.color="red";
+    colrChange.style.display = "flex";
+    colrChange.style.justifyContent = "end";
+})
