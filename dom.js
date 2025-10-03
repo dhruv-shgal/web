828d7e09ba5309cfac28d7daaa7c809af3651544
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-10-03 22:34:30 +0530

    Initial commit

--- dom.js
+++ dom.js
@@ -1,7 +1,5 @@
-let colrChange = document.getElementById("name");
-let btn=document.getElementById("change")
-btn.addEventListener("click",function(){
-    colrChange.style.color="red";
-    colrChange.style.display = "flex";
-    colrChange.style.justifyContent = "end";
+let add1=document.getElementById("btn");
+let two=document.getElementById("list");
+add1.addEventListener("click",function(){
+    two.appendChild("")
 })
