0e1010278fd2815d2a2864acdf4253bdfc67e14a
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-10-06 19:41:14 +0530

    Initial commit

--- try.js
+++ try.js
@@ -2,9 +2,25 @@
 // let x =10;
 // console.log(x);
 
-var a = 100;
-{
-    var a=10;
-    console.log(a);
+// var a = 100;
+// {
+//     var a=10;
+//     console.log(a);
+// }
+// console.log(a);
+
+async function greet(){
+    console.log("hello ....your name is ");
+    for(let i=0;i<3;i++){
+        setTimeout(()=>{
+        if (i==2){
+            console.log("Dhruv");
+        }
+        else{
+            console.log("um....");
+        }
+    },i*1000)
+    }
+    console.log("your age is 20");
 }
-console.log(a);
+greet();
